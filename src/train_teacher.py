b99f0ad6d090753227895710730aa283aaafba91
untitledunmastered1998 <106276401+untitledunmastered1998@users.noreply.github.com>
2022-09-25 15:15:19 +0800

    Update train_teacher.py

--- src/train_teacher.py
+++ src/train_teacher.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
                         help='Random seed (default=%(default)s)')
     parser.add_argument('--save-models', action='store_false',
                         help='Save trained models (default=%(default)s)')
-    parser.add_argument('--t-path', type=str, default='/home/data3/jskj_taozhe/DistillationTest/CIFAR100/T_models/',
+    parser.add_argument('--t-path', type=str, default='/',
                         help='Save teacher model pth file(default=%(default)s)')
 
     # 数据集参数 dataset args
